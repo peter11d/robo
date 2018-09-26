8374d108df5158f1a53780e483760778847e379d
Peter Drake <PeterDrake@dyn-160-39-131-29.dyn.columbia.edu>
2018-09-25 23:56:56 -0400

    Messing around

--- robotshw2.py
+++ robotshw2.py
@@ -3,44 +3,70 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+vel_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=1)
+
+def shutdown():
+    vel_pub.publish(Twist())
+    rospy.sleep(1)
+    
 def scan_callback(msg):
     global g_range_ahead
     g_range_ahead = min(msg.ranges)
     print(g_range_ahead)
-    #range_left = msg.ranges[-1]
-    #range_right = msg.ranges[0]
-    #range_ahead = msg.ranges[len(msg.ranges)/2]
-    
-    #print [range_left, range_ahead, range_right]
 
-g_range_ahead = 1
-
-
-
-#rospy.init_node('range_ahead')
-#scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
-#rospy.spin()
 scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
-cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=1)
-rospy.init_node('wander')
-state_change_time = rospy.Time.now()
-driving_forward = True
-rate = rospy.Rate(10)
-while not rospy.is_shutdown():
-    if driving_forward:
-        if (g_range_ahead < 0.8 or rospy.Time.now() > state_change_time):
-            driving_forward = False
-            state_change_time = rospy.Time.now() + rospy.Duration(5)
-    else: # we're not driving_forward
-        if rospy.Time.now() > state_change_time:
-            driving_forward = True 
-            # we're done spinning, time to go forward!
-            state_change_time = rospy.Time.now() + rospy.Duration(30)
-    twist = Twist()
-    if driving_forward:
-        twist.linear.x = 1
-    else:
-        twist.angular.z = 1
-    cmd_vel_pub.publish(twist)
 
-rate.sleep()
+
+
+rospy.init_node('bug', anonymous=False)
+
+state_change_time = rospy.Time.now()
+rate = rospy.Rate(10)
+linear_speed = 0.2
+
+def move_x(dist):
+    cmd = Twist()
+    cmd.linear.x = linear_speed
+    vel_pub.publish(cmd)
+    rospy.sleep(1)
+    
+def move_y(dist):
+    pass
+
+def move_z(dist):
+    pass
+    
+
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+move_x(1)
+
+
+
+#    (position, rotation) = self.get_odom()
+#    x_start = position.x
+#    y_start = position.y
+#    
+#    distance = 0
+
+#while not rospy.is_shutdown():
+#    if driving_forward:
+#        if (g_range_ahead < 0.8 or rospy.Time.now() > state_change_time):
+#            driving_forward = False
+#            state_change_time = rospy.Time.now() + rospy.Duration(5)
+#    else: # we're not driving_forward
+#        if rospy.Time.now() > state_change_time:
+#            driving_forward = True 
+#            # we're done spinning, time to go forward!
+#            state_change_time = rospy.Time.now() + rospy.Duration(30)
+#    twist = Twist()
+#    move_x(2, twist)
+#    cmd_vel_pub.publish(twist)
+
+shutdown()
